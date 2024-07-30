df087ca64ce5ec0904d78215e43ca19ac8e68c80
Lane Enget <enget006@umn.edu>
2024-07-30 16:42:23 -0500

    Implement div_by_seven

--- easy_exercises.py
+++ easy_exercises.py
@@ -70,7 +70,7 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 def mergeTwoLists(self, list1, list2):
         """
         :type list1: Optional[ListNode]
@@ -98,3 +98,22 @@ def mergeTwoLists(self, list1, list2):
             list3 = list3.next
 
         return ret.next
+
+# Write a program which will find all such numbers which are divisible 
+# by 7 but are not a multiple of 5, between 2000 and 3200 (both included).
+# The numbers obtained should be printed in a comma-separated sequence 
+# on a single line.
+def div_by_seven():
+
+    div = []
+    for num in range(2000, 3200):
+        if num % 7 == 0 and num % 5 != 0:
+            div.append(str(num))
+
+    print(','.join(div))
+
+# Write a program which can compute the factorial of a given
+# numbers. The results should be printed in a comma-separated
+# sequence on a single line.
+def factorial(n):
+    pass
